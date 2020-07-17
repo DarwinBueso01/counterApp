ec08f1145f25d8b191a57d63ac5069354f3b24ab
Darwin Bueso <darwingaldamez5@gmail.com>
2020-07-17 00:24:48 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import Counter from "./Counter";
+class App extends Component {
+  //INTALIZATION
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  incrementCounter = () => {
+    let currentCountValue = this.state.counter
+    this.setState({
+      counter: ++currentCountValue
+    })
+  }
+
+  resetCounter = () => {
+    this.setState({
+      counter: 0
+    })
+  }
+
+  // MOUNTING & UPDATING
+  render() {
+    return (
+      <div className="main--container">
+        <div className="counter--border">
+          <div className="counter--container">
+            <Counter counter={this.state.counter} />
+            <button onClick={this.incrementCounter}>Click Me!</button>
+            <button className="resetButton" onClick={this.resetCounter}>Reset</button>
+          </div>
+        </div>
+        <div className="sih--div--green"></div>
+        <div className="sih--div--orange"></div>
+      </div>
+    );
+  }
+}
+export default App;
